2ff33a329c23ea12ae78af819b7e240f2c56e49d
Bensi5 <77843600+Bensi5@users.noreply.github.com>
2021-11-16 17:25:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,51 @@
 img = "";
-status="";
-object=[];
+status = "";
+object = [];
 function preload() {
     img = loadImage("dog_cat.jpg");
 }
 function setup() {
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(380,380);
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Object";
 }
 function draw() {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
     console.log("image_draw");
-    if(status!= ""){
+    objectDetector.detect(video, getResults);
+    if (status != "") {
         console.log(" in status");
-        for(i=0;i<object.length;i++){
+        for (i = 0; i < object.length; i++) {
             document.getElementById("status").innerHTML = "Status : Object Detected ";
-
-            fill("red");
-            percent=floor(object[i].confidence*100);
-            text(object[i].label+" "+percent+"%",object[i].x+10,object[i].y+20);
+            document.getElementById("number_of_object").innerHTML = "Number of objects detected are : " + object.length;
+            r = random(255);
+            g = random(255);
+            b = random(255);
+            fill(r, g, b);
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", object[i].x + 10, object[i].y + 20);
             noFill();
-            stroke("red");
-            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+            stroke(r, g, b);
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
         }
     }
-   
+
 }
-function modelLoaded(){
+function modelLoaded() {
     console.log('Model is loaded');
-    status=true;
-    objectDetector.detect(img,getResults);
+    status = true;
+
 }
-function getResults(error,results){
-    if(error){
+function getResults(error, results) {
+    if (error) {
         console.log(error);
     }
-    else{
+    else {
         console.log(results);
-        object=results;
+        object = results;
     }
 }
